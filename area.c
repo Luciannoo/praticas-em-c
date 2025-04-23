1b813635fbe382c581cb1dcbc2c5790824e2c907
Ribeiro <luciano.ribeirodev@gmail.com>
2025-04-23 22:34:14 +0000

    commit da pratica de operadores aritmeticos

--- area.c
+++ area.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+int main(){
+    //Cálculo de Área e Perímetro de um Retângulo
+    float altura, largura, area, perimetro;
 
+    printf("Altura do retângulo: "); 
+    scanf("%f", &altura);
+
+    printf("Largura do retângulo: ");
+    scanf("%f", &largura);
+
+    //calculando a área
+    area = altura * largura;
+    printf("A aréa do retângulo é: %.2f\n", area);
+
+    //calculando o perimetro
+    perimetro = 2 * (altura + largura);
+    printf("O perímetro do retângulo é: %.2f", perimetro);
+}
